897cf26128cbfefa4699a87fc3302ea3d299d47e
soynog <nicolas.o.garcia@gmail.com>
2016-04-14 23:08:19 -0400

    Add Multiple Xfr Function
    - Does not work in all cases -- needs to pick among equidistant transfer points

--- lib/practice.rb
+++ lib/practice.rb
@@ -7,9 +7,10 @@ module MBTA
 
       def initialize
         @lines = {
-          'Red' => Line.new('South Station', 'Park Street', 'Kendall',
-                            'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'),
-          'Green' => Line.new('Haymarket', 'Government Center',
+          'Red' => Line.new('Red', 'South Station', 'Park Street',
+                            'Kendall', 'Central', 'Harvard', 'Porter',
+                            'Davis', 'Alewife'),
+          'Green' => Line.new('Green', 'Haymarket', 'Government Center',
                               'Park Street', 'Boylston', 'Arlington',
                               'Copley', 'Hynes', 'Kenmore'),
           'Orange' => Line.new('Orange', 'North Station', 'Haymarket',
@@ -23,41 +24,31 @@ module MBTA
         if line_a == line_b
           @lines[line_a].dist_on_line(station_a, station_b)
         else
-          @lines[line_a].dist_on_line(station_a, nearest_xfr) +
-            @lines[line_b].dist_on_line(station_b, nearest_xfr)
+          xfr = nearest_xfr(line_a, station_a, line_b)
+          @lines[line_a].dist_on_line(station_a, xfr) +
+            @lines[line_b].dist_on_line(station_b, xfr)
         end
       end
 
       # Returns the nearest connection point between two lines
-      def nearest_xfr # (line_a, station, line_b)
-        'Park Street'
-        # xfr_stns(line_a, line_b).min_by do |xfr_stn|
-        #   dist_on_line(line_a, station, xfr_stn)
-        # end
+      def nearest_xfr(line_a, station, line_b)
+        # 'Park Street'
+        @lines[line_a].xfr_stns(@lines[line_b]).min_by do |xfr_stn|
+          @lines[line_a].dist_on_line(station, xfr_stn)
+        end
       end
-
-      # # Returns an array of transfers between two lines
-      # def xfr_stns(line_a, line_b)
-      #   @lines[line(line_a)].select do |stn_i|
-      #     xfr?(line_a, stn_i, line_b)
-      #   end
-      # end
-      #
-      # # Returns true if station is a transfer point between lines
-      # def xfr?(line_a, station, line_b)
-      #   @lines[line(line_a)].include?(stn(station)) &&
-      #     @lines[line(line_b)].include?(stn(station))
-      # end
 
       # return true if handling multiple intersections
       def self.stretch
+        true
       end
     end
     # One line, all the stations on that line
     class Line
       attr_reader :name, :stns
 
-      def initialize(*stns)
+      def initialize(name, *stns)
+        @name = name
         @stns = stns.map do |stn|
           Station.new(stn)
         end
@@ -68,10 +59,26 @@ module MBTA
         @stns.index(station)
       end
 
+      def include?(station)
+        @stns.include?(station)
+      end
+
       # Returns distance between two stops on a line
       def dist_on_line(stn_a, stn_b)
         return nil unless stop_num(stn_a) && stop_num(stn_b)
         (stop_num(stn_a) - stop_num(stn_b)).abs
+      end
+
+      # Returns true if station is a transfer point to the passed line
+      def xfr?(station, other_line)
+        other_line.include?(station)
+      end
+
+      # Returns an array of transfers to the other line
+      def xfr_stns(other_line)
+        @stns.select do |stn_i|
+          xfr?(stn_i, other_line)
+        end
       end
     end
     # One station
