9f6c2e2bfce6aed927f8cc3f34808bb993e91c14
MishaHerscu <mishaherscu@gmail.com>
2016-06-12 19:46:07 -0400

    added another  of my own tests

--- lib/my_tests.rb
+++ lib/my_tests.rb
@@ -11,5 +11,9 @@ park_alewife = my_mbta.stops_between_stations('Green', 'Park Street',
 haymarket_north = my_mbta.stops_between_stations('Green', 'Haymarket',
                                                  'Orange', 'North Station')
 
+gov_state = my_mbta.stops_between_stations('Green', 'Government Center',
+                                           'Orange', 'State')
+
 p "Park Street to Alewife (6): #{park_alewife}"
 p "Haymarket to North Station (1): #{haymarket_north}"
+p "Gov to State (2): #{gov_state}"
