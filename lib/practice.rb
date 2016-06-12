7a54dc1d48b37dedaed14e8c7bf64751dda17e3b
MishaHerscu <mishaherscu@gmail.com>
2016-06-12 17:23:55 -0400

    rake test passes

--- lib/practice.rb
+++ lib/practice.rb
@@ -4,9 +4,28 @@ module MBTA
     # All of the subway lines
     class System
       def initialize
+        @red = ['South Station', 'Park Street', 'Kendall',
+                'Central', 'Harvard', 'Porter', 'Davis', 'Alewife']
+        @green = ['Government Center', 'Park Street', 'Boylston', 'Arlington',
+                  'Copley', 'Hynes', 'Kenmore']
+        @orange = ['North Station', 'Haymarket', 'Park Street', 'State',
+                   'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
+        @translator = { 'Red' => @red,
+                        'Green' => @green,
+                        'Orange' => @orange }
       end
 
-      def stops_between_stations
+      def stops_between_stations(start_line,
+                                 start_station,
+                                 stop_line,
+                                 stop_station)
+        start_line = @translator[start_line]
+        stop_line = @translator[stop_line]
+        first_distance = (start_line.index(start_station) -
+        start_line.index('Park Street')).abs
+        second_distance = (stop_line.index(stop_station) -
+        stop_line.index('Park Street')).abs
+        first_distance + second_distance
       end
 
       # return true if handling multiple intersections
@@ -18,6 +37,11 @@ module MBTA
     end
     # One station
     class Station
+      # attr_reader @line, @position
+      # def initialize(line, position)
+      #   @line = line
+      #   @position = position
+      # end
     end
   end
 end
