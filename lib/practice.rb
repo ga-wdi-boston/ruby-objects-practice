24521779484d672d86d2f8da7d4202e1be96c658
mattb5 <mattbuffardi@gmail.com>
2016-06-12 19:45:22 -0400

    first few working methods

--- lib/practice.rb
+++ lib/practice.rb
@@ -1,23 +1,38 @@
+require 'pry'
+
 # A namespace
-module MBTA
-  module Subway
-    # All of the subway lines
-    class System
-      def initialize
-      end
+# module MBTA
+# module Subway
+# All of the subway lines
+class System
+  def initialize
+    @red_line = ['South Station', 'Park Street', 'Kendall']
+  end
 
-      def stops_between_stations
-      end
+  def get_red_line
+    @red_line
+  end
 
-      # return true if handling multiple intersections
-      def self.stretch
-      end
-    end
-    # One line, all the stations on that line
-    class Line
-    end
-    # One station
-    class Station
-    end
+  def stops_between_stations(startStation, stopStation)
+    stops =
+  end
+
+  # return true if handling multiple intersections
+  def self.stretch
   end
 end
+# One line, all the stations on that line
+class Line
+  def initialize(line)
+    @line = line
+  end
+
+  def get_line
+    @line
+  end
+end
+# One station
+class Station
+end
+#   end
+# end
