b55ee2e76ef572e4f87620118ddfdb47785cbe62
erinsul <erin_sullivan50@yahoo.com>
2016-06-13 08:06:06 -0400

    Add stations and functions to find distance between them.
    Yes, I really should've been committing more, but I got distracted.

--- lib/practice.rb
+++ lib/practice.rb
@@ -3,10 +3,37 @@ module MBTA
   module Subway
     # All of the subway lines
     class System
+  attr_reader :red, :green, :orange
       def initialize
+          @red = {name: 'Red', stops: ['South Station', 'Park Street', 'Kendall',
+             'Central', 'Harvard', 'Porter', 'Davis', 'Alewife']}
+          @green = {name: 'Green', stops: ['Government Center', 'Park Street', 'Boylston',
+            'Arlington', 'Copley', 'Hynes', 'Kenmore']}
+          @orange = {name: 'Orange', stops: ['North Station', 'Haymarket', 'Park Street',
+            'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']}
+      end
+
+      def get_line(line)
+        if line == 'Red'
+          return red
+        elsif line == 'Green'
+          return green
+        elsif line == 'Orange'
+        return orange
+      end
       end
 
       def stops_between_stations(start_line, start_station, end_line, end_station)
+          if start_line == end_line
+            (get_line(start_line)[:stops].index(start_station) -
+            get_line(start_line)[:stops].index(end_station)).abs
+          else
+            @line1_dist = (get_line(start_line)[:stops].index(start_station) -
+            get_line(start_line)[:stops].index('Park Street')).abs
+            @line2_dist = (get_line(end_line)[:stops].index(end_station) -
+            get_line(end_line)[:stops].index('Park Street')).abs
+            @line2_dist + @line1_dist
+          end
       end
 
       # return true if handling multiple intersections
@@ -37,31 +64,5 @@ module MBTA
         @@stations.select { |station| station.line == name }
       end
     end
-    south = Station.new('South Station', 'Red')
-    kendall = Station.new('Kendall MIT', 'Red')
-    central = Station.new('Central', 'Red')
-    harvard = Station.new('Harvard', 'Red')
-    porter = Station.new('Porter', 'Red')
-    davis = Station.new('Davis', 'Red')
-    alewife = Station.new('Alewife', 'Red')
-    government = Station.new('Government Center', 'Green')
-    boylston = Station.new('Boylston', 'Green')
-    arlington = Station.new('Arlington', 'Green')
-    copley = Station.new('Copley', 'Green')
-    hynes = Station.new('Hynes Convention Center', 'Green')
-    kenmore = Station.new('Kenmore', 'Green')
-    north = Station.new('North Station', 'Orange')
-    haymarket = Station.new('Haymarket', 'Orange')
-    state = Station.new('State', 'Orange')
-    downtown = Station.new('Downtown Crossing', 'Orange')
-    chinatown = Station.new('Chinatown', 'Orange')
-    back_bay = Station.new('Back Bay', 'Orange')
-    forest_hills = Station.new('Forest Hills', 'Orange')
-    red = Line.new('Red')
-    red.add_stations
-    green = Line.new('Green')
-    green.add_stations
-    orange = Line.new('Orange')
-    orange.add_stations
   end
 end
