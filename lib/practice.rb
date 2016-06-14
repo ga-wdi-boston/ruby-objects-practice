ee59bf04bc848f20c4cc5b6183ffbb340c692550
shkherad <shkherad@bu.edu>
2016-06-13 20:15:23 -0400

    data structure established
    just write functions and test

--- lib/practice.rb
+++ lib/practice.rb
@@ -6,18 +6,73 @@ module MBTA
       def initialize
       end
 
-      def stops_between_stations
+      def stops_between_stations(start_line,start_station,end_line,end_station)
+
       end
 
       # return true if handling multiple intersections
       def self.stretch
       end
     end
+
     # One line, all the stations on that line
     class Line
+      attr_reader :color
+      attr_accessor :stations
+      @@lines=[]
+      def initialize(color)
+        @color= color
+        @stations = []
+        @@lines<<self
+      end
+      def self.all
+        @@lines
+      end
     end
+
+
     # One station
     class Station
+      attr_reader :name, :position
+      def initialize(position:,name:)
+        @position = position
+        @name = name
+      end
     end
+
   end
 end
+
+
+red_line = Line.new("Red")
+red_line.stations<<Station.new(position:0,station:"South Station")
+red_line.stations<<Station.new(position:1,station:"Park Street")
+red_line.stations<<Station.new(position:2,station:"Kendall")
+red_line.stations<<Station.new(position:3,station:"Central")
+red_line.stations<<Station.new(position:4,station:"Harvard")
+red_line.stations<<Station.new(position:5,station:"Porter")
+red_line.stations<<Station.new(position:6,station:"Davis")
+red_line.stations<<Station.new(position:7,station:"Alewife")
+
+green_line = Line.new("Green")
+green_line.stations<<Station.new(position:0,station:"Government Center")
+green_line.stations<<Station.new(position:1,station:"Park Street")
+green_line.stations<<Station.new(position:2,station:"Boylston")
+green_line.stations<<Station.new(position:3,station:"Arlington")
+green_line.stations<<Station.new(position:4,station:"Copley")
+green_line.stations<<Station.new(position:5,station:"Hynes")
+green_line.stations<<Station.new(position:6,station:"Kenmore")
+
+orange_line = Line.new("Orange")
+orange_line.stations<<Station.new(position:0,station:"North Station")
+orange_line.stations<<Station.new(position:0,station:"Haymarket")
+orange_line.stations<<Station.new(position:0,station:"Park Street")
+orange_line.stations<<Station.new(position:0,station:"State")
+orange_line.stations<<Station.new(position:0,station:"Downtown Crossing")
+orange_line.stations<<Station.new(position:0,station:"Chinatown")
+orange_line.stations<<Station.new(position:0,station:"Back Bay")
+orange_line.stations<<Station.new(position:0,station:"Forest Hills")
+
+p red_line
+p orange_line
+p green_line
