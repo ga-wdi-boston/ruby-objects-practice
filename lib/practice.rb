aa2e24a5a80e26c2daa30dec77b47e6a7592a08c
shkherad <shkherad@bu.edu>
2016-06-14 16:54:48 -0400

    Line and Station class set up
    working code

--- lib/practice.rb
+++ lib/practice.rb
@@ -1,9 +1,17 @@
-# A namespace
-module MBTA
-  module Subway
+# # A namespace
+# module MBTA
+  # module Subway
     # All of the subway lines
+
+
+
     class System
-      def initialize
+      @@subway = []
+
+      def initialize(city)
+        @city = city
+        @lines = []
+        @@subway<<self
       end
 
       def stops_between_stations(start_line,start_station,end_line,end_station)
@@ -33,16 +41,18 @@ module MBTA
 
     # One station
     class Station
-      attr_reader :name, :position
-      def initialize(position:,name:)
+      attr_reader :station, :position
+      def initialize(position:,station:)
         @position = position
-        @name = name
+        @station = station
       end
     end
 
-  end
-end
+#   end
+# end
 
+
+mbta = System.new("Boston")
 
 red_line = Line.new("Red")
 red_line.stations<<Station.new(position:0,station:"South Station")
@@ -65,14 +75,16 @@ green_line.stations<<Station.new(position:6,station:"Kenmore")
 
 orange_line = Line.new("Orange")
 orange_line.stations<<Station.new(position:0,station:"North Station")
-orange_line.stations<<Station.new(position:0,station:"Haymarket")
-orange_line.stations<<Station.new(position:0,station:"Park Street")
-orange_line.stations<<Station.new(position:0,station:"State")
-orange_line.stations<<Station.new(position:0,station:"Downtown Crossing")
-orange_line.stations<<Station.new(position:0,station:"Chinatown")
-orange_line.stations<<Station.new(position:0,station:"Back Bay")
-orange_line.stations<<Station.new(position:0,station:"Forest Hills")
+orange_line.stations<<Station.new(position:1,station:"Haymarket")
+orange_line.stations<<Station.new(position:2,station:"Park Street")
+orange_line.stations<<Station.new(position:3,station:"State")
+orange_line.stations<<Station.new(position:4,station:"Downtown Crossing")
+orange_line.stations<<Station.new(position:5,station:"Chinatown")
+orange_line.stations<<Station.new(position:6,station:"Back Bay")
+orange_line.stations<<Station.new(position:7,station:"Forest Hills")
 
-p red_line
-p orange_line
-p green_line
+
+start_station = red_line.stations.select { |stops| stops.station=='Central'}[0].position
+p start_station
+# p orange_line
+# p green_line
