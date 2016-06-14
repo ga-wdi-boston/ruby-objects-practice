ccea4e53df82eff62e1e6461572ac92cb7139cd8
mattb5 <mattbuffardi@gmail.com>
2016-06-13 21:34:25 -0400

    about to add elsif statement

--- lib/practice.rb
+++ lib/practice.rb
@@ -7,43 +7,50 @@ require 'pry'
 class System
   def initialize
     @red = ['South Station', 'Park Street', 'Kendall']
+    @green = ['Government Center', 'Park Street', 'Boylston']
   end
 
   # def stops_between_stations(startStation, startLine, stopStation, stopLine)
-  def stops_between_stations(startLine,stopLine)
-  start_line = return_line(startLine)
-  stop_line = return_line(stopLine)
-  
-    # start_index = get_station(startStation).index(stopStation.get_line)
-  end
+  def stops_between_stations(startStation, startLine, stopStation, stopLine)
+    start_line = return_line(startLine)
+    stop_line = return_line(stopLine)
 
+    if start_line == stop_line
+    stops = start_line.index(startStation) - start_line.index(stopStation)
+    else
+
+    end
+    puts stops
+  end
 
   def return_line(station)
     if station == 'Red Line'
       return @red
+    elsif station == 'Green Line'
+      return @green
     end
   end
 end
 # One line, all the stations on that line
-class Line
-  def initialize(line)
-    @line = line
-  end
-
-  def get_line
-    @line
-  end
-end
-# One station
-class Station
-  def initialize(station)
-    @station = station
-  end
-
-  def get_station
-    @station
-  end
-end
+# class Line
+#   def initialize(line)
+#     @line = line
+#   end
+#
+#   def get_line
+#     @line
+#   end
+# end
+# # One station
+# class Station
+#   def initialize(station)
+#     @station = station
+#   end
+#
+#   def get_station
+#     @station
+#   end
+# end
 
 mbta = System.new
-mbta.stops_between_stations("Red Line","Red Line")
+mbta.stops_between_stations("Kendall","Red Line","Park Street","Red Line")
