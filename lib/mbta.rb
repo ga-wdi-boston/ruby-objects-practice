53cdf2ed50cd7f8b185f6d97296a0746b1d489af
DanielJRaine <ley.danielj@gmail.com>
2016-09-15 18:51:24 -0400

    Add skeleton of functions

---
+++ lib/mbta.rb
@@ -0,0 +1,35 @@
+require 'practice.rb'
+south_station = Station.new('south station')
+park_street = Station.new('park street')
+kendall = Station.new('kendall')
+central = Station.new('central')
+harvard = Station.new('harvard')
+porter = Station.new('porter')
+davis = Station.new('davis')
+alewife = Station.new('alewife')
+
+government_center = Station.new('government center')
+park_street
+boylston = Station.new('boylston')
+arlington = Station.new('arlington')
+copley = Station.new('copley')
+hynes = Station.new('hynes')
+kenmore = Station.new('kenmore')
+
+north_station = Station.new('north station')
+haymarket = Station.new('haymarket')
+park_street
+state = Station.new('state')
+downtown_crossing = Station.new('downtown crossing')
+chinatown = Station.new('chinatown')
+back_bay = Station.new('back bay')
+ruggles = Station.new('ruggles')
+forest_hills = Station.new('forest hills')
+
+red_line = Line.new([south_station, park_street, kendall, central, harvard, porter, davis, alewife])
+
+green_line = Line.new([government_center, park_street, boylston, arlington, copley, hynes, kenmore])
+
+orange_line = Line.new([north_station, haymarket, park_street, state, downtown_crossing, chinatown, back_bay, ruggles, forest_hills])
+
+mbta = System.new([red_line, green_line, orange_line])
--- lib/practice.rb
+++ lib/practice.rb
@@ -1,23 +1,53 @@
 # A namespace
-module MBTA
-  module Subway
+# module MBTA
+#   module Subway
     # All of the subway lines
     class System
-      def initialize
+      def initialize(lines)
+        @subway = lines
       end
 
       def stops_between_stations
+        direction = get_direction(start_station, end_station)
+        if station_on_same_line?(start_station, end_station)
+          stops_between = count_stops(start_station, end_station)
+        else
+          transfer # returns transfer station
+          stops_between = count_stops(start_station, transfer_station)
+          stops_between += count_stops(transfer_station, final_station)
+        end
+        stops_between
       end
-
+      
+      def get_direction(start_station, end_station)
+      end
+      
+      def station_on_same_line?(start_station, end_station)
+      end
+      
+      def stops_between(start_station, end_station)
+      end
+      
+      def count_stops(start_station, end_station)
+      end
       # return true if handling multiple intersections
       def self.stretch
       end
     end
+    
+    
     # One line, all the stations on that line
     class Line
+      def initialize(stations)
+        @stations = stations
+      end
     end
     # One station
     class Station
+      attr_reader :name
+      def initialize(name)
+        @name = name
+      end
     end
-  end
-end
+#   end
+# end
