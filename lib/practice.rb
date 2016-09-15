29256a2a41b07b5d46c863974c07ef27f7784a32
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-15 19:29:22 -0400

    Complete practice
    Complete practice

--- lib/practice.rb
+++ lib/practice.rb
@@ -3,10 +3,41 @@ module MBTA
   module Subway
     # All of the subway lines
     class System
+      attr_reader :lines
       def initialize
+        @lines = {
+          Green: ['Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore'],
+          Red: ['South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+          Orange: ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
+        }
       end
 
-      def stops_between_stations
+      def stops_between_stations(
+        start_line,
+        start_station,
+        end_line,
+        end_station
+      )
+        start_index = @lines[start_line.to_sym].index(start_station)
+        end_index = @lines[end_line.to_sym].index(end_station)
+        num_stops = 0
+
+        if start_line == end_line
+          # get the absolute value of the difference between indices
+          num_stops = (start_index - end_index).abs
+        else
+          # get number of stops from start station to Park
+          start_park_index = @lines[start_line.to_sym].index('Park Street')
+          first_leg = (start_index - start_park_index).abs
+
+          # get number of stops from Park to end station
+          end_park_index = @lines[end_line.to_sym].index('Park Street')
+          second_leg = (end_park_index - end_index).abs
+
+          # add those numbers
+          num_stops = first_leg + second_leg
+        end
+        num_stops
       end
 
       # return true if handling multiple intersections
