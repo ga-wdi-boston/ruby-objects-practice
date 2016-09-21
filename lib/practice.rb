0c5393f83063652764482e1d88ecc0f380c5175c
julie1013 <julie1380@verizon.net>
2016-09-21 15:59:53 -0400

    Completed
    Completed, passed all tests.

--- lib/practice.rb
+++ lib/practice.rb
@@ -3,52 +3,46 @@ module MBTA
   module Subway
     # All of the subway lines
     class System
-      def initialize(start_line, start_station, end_line, end_station)
-        @start_line = start_line
-        @start_station = start_station
-        @end_line = end_line
-        @end_station = end_station
+      attr_reader :lines
+      def initialize
+        @lines = {
+          Red:    ['South Station', 'Park Street', 'Kendall', 'Central',
+                   'Harvard', 'Porter', 'Davis', 'Alewife'],
+          Green:  ['Government Center', 'Park Street', 'Boylston',
+                   'Arlington', 'Copley', 'Hynes', 'Kenmore'],
+          Orange: ['North Station', 'Haymarket', 'Park Street', 'State',
+                   'Downtown Crossing', 'Chinatown', 'Back Bay',
+                   'Forest Hills']
+                 }
       end
 
-      def single_line_ride
-        (start_line.index(end_station) - start_line.index(start_station)).abs
-      end
+            def stops_between_stations(
+              start_line,
+              start_station,
+              end_line,
+              end_station
+            )
 
-      def single_transfer(start_line, start_station, end_line, end_station)
-        first_leg = (start_line.index('Park Street') -
-                     start_line.index(start_station)).abs
-        second_leg = end_line.index(end_station) - end_line.index('Park Street')
-        result = first_leg + second_leg
-        puts result
-      end
+              start_index = @lines[start_line.to_sym].index(start_station)
+              end_index = @lines[end_line.to_sym].index(end_station)
+              num_stops = 0
 
-      def stops_between_stations
-        if start_line == end_line
-          number_of_stops = single_line_ride(start_line, start_station,
-                                             end_line, end_station)
-        else
-          number_of_stops = single_transfer(start_line, start_station, end_line,
-                                            end_station)
-        end
-        puts number_of_stops
-      end
+              if start_line == end_line
+                num_stops = (end_index - start_index).abs
+              else
+                first_park_index = @lines[start_line.to_sym]
+                                   .index('Park Street')
+                first_leg = (first_park_index - start_index).abs
+                second_park_index = @lines[end_line.to_sym].index('Park Street')
+                second_leg = (end_index - second_park_index).abs
 
-      # return true if handling multiple intersections
-      def self.stretch
-      end
-    end
-    # One line, all the stations on that line
-    class Line
-      # red_line = ['South Station', 'Park Street', 'Kendall', 'Central',
-      #             'Harvard', 'Porter', 'Davis', 'Alewife']
-      # green_line = ['Government Center', 'Park Street', 'Boylston',
-      #               'Arlington', 'Copley', 'Hynes', 'Kenmore']
-      # orange_line = ['North Station', 'Haymarket', 'Park Street', 'State',
-      #                'Downtown Crossing', 'Chinatown', 'Back Bay',
-      #                'Forest Hills']
-    end
-    # One station
-    class Station
+                num_stops = first_leg + second_leg
+              end
+              num_stops
+            end
+
+            def self.stretch
+            end
     end
   end
 end
