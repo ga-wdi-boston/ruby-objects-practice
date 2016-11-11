72928bcaacce09ff4234baa126c1dcf8040e6b84
paulsevere <pjo2110@columbia.edu>
2016-11-11 14:46:17 -0500

    worked on practice

--- lib/practice.rb
+++ lib/practice.rb
@@ -3,11 +3,24 @@
 module MBTA
   module Subway
     # All of the subway lines
+
     class System
-      def initialize
+      attr_accessor :lines
+      def initialize(lines)
+        lines_object = {}
+        lines.each { |line| lines_object[line.name.to_sym] = line }
+        @lines = lines_object
       end
 
-      def stops_between_stations
+      def stops_between_stations(_start_line, _start_stop, _end_line, _end_stop)
+      end
+
+      def flatten!
+        stops = []
+        @lines.each do |line|
+          puts line
+        end
+        # stops.flatten!
       end
 
       # return true if handling multiple intersections
@@ -16,9 +29,19 @@ module MBTA
     end
     # One line, all the stations on that line
     class Line
+      attr_reader :stops, :name
+      def initialize(name, stops)
+        @name = name
+        @stops = stops.map.with_index { |stop_name, index| Station.new(stop_name, index) }
+      end
     end
     # One station
     class Station
+      attr_reader :name, :index
+      def initialize(name, index)
+        @name = name.strip
+        @index = index
+      end
     end
   end
 end
