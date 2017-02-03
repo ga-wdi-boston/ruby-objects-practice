29a2938bea77f41596979ea205c55cdcb30f85e2
Sofistication <sordonez93@gmail.com>
2017-02-03 18:36:02 -0500

    Start defining things

--- lib/practice.rb
+++ lib/practice.rb
@@ -4,10 +4,12 @@ module MBTA
   module Subway
     # All of the subway lines
     class System
+      LINES = [].map(&:freeze)
       def initialize
+        # green = Line.new(['Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore'])
       end
 
-      def stops_between_stations
+      def stops_between_stations(initial_line, initial_stop, final_line, final_stop)
       end
 
       # return true if handling multiple intersections
@@ -16,9 +18,20 @@ module MBTA
     end
     # One line, all the stations on that line
     class Line
+      attr_reader :name, :stops
+
+      def initialize(name, stops)
+        @name = name
+        @stops = stops
+      end
     end
     # One station
     class Station
+      attr_reader :name
+
+      def initialize(name)
+        @name = name
+      end
     end
   end
 end
