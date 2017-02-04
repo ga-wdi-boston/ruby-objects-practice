f7df96f04cf30df8ec5945886c30e9f749208d34
akgoode <akgoode@gmail.com>
2017-02-04 16:26:10 -0500

    Add response to practice
    Comfort: 5
    Clarity: 3
    
    It was really hard to figure out what this question was asking and to implement it based on the code that was provided.  I didn't use self.stretch and I think it would have been easier had I started from scratch.  I used the Array, String, and Number Ruby documentation

--- lib/practice.rb
+++ lib/practice.rb
@@ -4,18 +4,45 @@ module MBTA
   module Subway
     # All of the subway lines
     class System
+      attr_reader :stations, :intersection
       def initialize
+        @stations = %w(red green orange)
+        @intersection = 'Park Street'
       end
 
-      def stops_between_stations
+      def stops_between_stations(on_line, on_stop, off_line, off_stop)
+        start_line = Line.new(on_line)
+        finish_line = Line.new(off_line)
+        if on_line.eql? off_line
+          (start_line.line.find_index(on_stop) - finish_line.line.find_index(off_stop)).abs
+        else
+          on_line_stops = (start_line.line.find_index(on_stop) - start_line.line.find_index(self.intersection)).abs
+          off_line_stops = (finish_line.line.find_index(self.intersection) - finish_line.line.find_index(off_stop)).abs
+          on_line_stops + off_line_stops
+        end
       end
-
-      # return true if handling multiple intersections
+      # stretch method
       def self.stretch
       end
     end
     # One line, all the stations on that line
     class Line
+      RED = ['South Station', 'Park Street', 'Kendall', 'Central',
+             'Harvard', 'Porter', 'Davis', 'Alewife'].freeze
+      GREEN = ['Government Center', 'Park Street', 'Boylston', 'Arlington',
+               'Copley', 'Hynes', 'Kenmore'].freeze
+      ORANGE = ['North Station', 'Haymarket', 'Park Street', 'State',
+                'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills'].freeze
+      attr_reader :line
+      def initialize(line)
+        if line.eql? 'Red'
+          @line = RED
+        elsif line.eql? 'Green'
+          @line = GREEN
+        else
+          @line = ORANGE
+        end
+      end
     end
     # One station
     class Station
