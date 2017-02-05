409d14e6a3a54aefa2e6397dfc6490970b753b5f
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-02-05 14:41:22 -0500

    Study - Ruby Objects practice
    Comfort: 3
    Clarity: 5
    
    I used ruby class, array documentations and the class repos for this.
    Please let me know how to code this better as I only use two methods,
    and not sure how to break the code down into pieces.

--- lib/practice.rb
+++ lib/practice.rb
@@ -5,9 +5,23 @@ module MBTA
     # All of the subway lines
     class System
       def initialize
+        @mbta = { Red: ['South Station', 'Park Street', 'Kendall',
+                        'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+                  Green: ['Government Center', 'Park Street', 'Boylston',
+                          'Arlington', 'Copley', 'Hynes', 'Kenmore'],
+                  Orange: ['North Station', 'Haymarket', 'Park Street', 'State',
+                           'Downtown Crossing', 'Chinatown', 'Backbay',
+                           'Forest Hills'] }
       end
-
-      def stops_between_stations
+      # method for calculating the amount of stations taken to destination
+      def stops_between_stations(start_color, start_point, end_color, end_point)
+        line1_start_station = @mbta[start_color.to_sym].index(start_point)
+        line1_end_station = @mbta[start_color.to_sym].index('Park Street')
+        line1 = (line1_start_station - line1_end_station).abs
+        line2_start_station = @mbta[end_color.to_sym].index('Park Street')
+        line2_end_station = @mbta[end_color.to_sym].index(end_point)
+        line2 = (line2_start_station - line2_end_station).abs
+        line1 + line2
       end
 
       # return true if handling multiple intersections
