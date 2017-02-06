fec053b9fe9cc98f85df2487ed85c322464266a4
Sofistication <sordonez93@gmail.com>
2017-02-05 22:41:28 -0500

    Almost working
    - I broke something but the calcs probalby work

--- lib/practice.rb
+++ lib/practice.rb
@@ -4,12 +4,22 @@ module MBTA
   module Subway
     # All of the subway lines
     class System
-      LINES = [].map(&:freeze)
+      # attr_reader :lines
+      LINES = [ Line.new('Green', ['Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore']),
+      Line.new('Red', ['South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife']),
+      Line.new('Orange', ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills'])].map(&:freeze)
+      # LINES = [].map(&:freeze)
       def initialize
-        # green = Line.new(['Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore'])
       end
 
       def stops_between_stations(initial_line, initial_stop, final_line, final_stop)
+        init = LINES.find { |line| line.name == initial_line }
+        final = LINES.find { |line| line.name == final_line }
+        if initial_line == final_line
+          init.stops_calc(initial_stop, final_stop)
+        else
+          init.stops_calc(initial_stop, 'Park Street') + final.stops_calc('Park Street', final_stop)
+        end
       end
 
       # return true if handling multiple intersections
@@ -22,15 +32,24 @@ module MBTA
 
       def initialize(name, stops)
         @name = name
-        @stops = stops
+        @stops = stops.map do |stop|
+          Station.new(stop, name)
+        end
+      end
+
+      def stops_calc(initial, final)
+        on = stops.index { |stop| stop.name == initial }
+        off = stops.index { |stop| stop.name == final }
+        (on - off).abs
       end
     end
     # One station
     class Station
-      attr_reader :name
+      attr_reader :name, :line
 
-      def initialize(name)
+      def initialize(name, line)
         @name = name
+        @line = line
       end
     end
   end
