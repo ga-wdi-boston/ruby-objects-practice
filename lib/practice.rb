cc29c896697374baf3bf2b6181e372261e5507a9
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-02-06 08:24:02 -0500

    create global variable and method method is for on and off of same line.

--- lib/practice.rb
+++ lib/practice.rb
@@ -1,24 +1,72 @@
 # frozen_string_literal: true
 # A namespace
+
+# isn't MBTA the Subway? what is the difference?
+# module MBTA
+# module Subway
+
+# module MBTA
 module MBTA
-  module Subway
-    # All of the subway lines
-    class System
-      def initialize
-      end
+  red = ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"]
+  green = ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"]
+  orange = ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
+  intersection = ["Park Street"]
+end
 
-      def stops_between_stations
-      end
+# class System
+class System
+  include MBTA
 
-      # return true if handling multiple intersections
-      def self.stretch
-      end
-    end
-    # One line, all the stations on that line
-    class Line
-    end
-    # One station
-    class Station
+  def initialize(red, green, orange, intersection, total_stops)
+    @red = red
+    @green = green
+    @orange = orange
+    @intersection = intersection
+    @total_stops = total_stops
+  end
+
+  # calculates the number of stops between stations on the "MBTA"
+  def stops_between_stations
+    unless same_line == false then different_lines
     end
   end
+
+  # return true if handling multiple intersections
+  def self.stretch=(station_one, station_two)
+    if line.includes?(station_one && station_two)
+      true
+    end
+  end
+
+  def different_lines=(station_one, station_two)
+    # need to figure a way to have arrays (lines) be connected to each other through the element Park_Street.
+  end
+end
+
+# One line, all the stations on that line
+class Line
+  attr_reader :same_line
+  include MBTA
+
+  def initialize(line, station_one, station_two)
+    @line = line
+    @station_one = station_one
+    @station_two = station_two
+  end
+
+  # calculates the number of stops between stations on same line
+  def same_line=(line, station_one, station_two)
+    if line.includes?(station_one && station_two)
+       on_index = line.index{ |x| x == station_one}
+       off_index = line.index{ |x| x == station_two}
+       total_stops = off_index - on_index
+       # returns total number of stops for the trip.
+       p "#{total_stops}"
+     end
+  end
+end
+
+# One station
+class Station
+  #what is purpose of this class?
 end
