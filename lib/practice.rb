acf618dde1c9a9fdc2bf709a539a0bc7b33553c9
jim-moody <jim.moody@libertymutual.com>
2017-04-16 10:49:49 -0400

    Update the Subway class to have an array of lines
    Better than creating an object to hold the lines

--- lib/practice.rb
+++ lib/practice.rb
@@ -1,48 +1,34 @@
 # frozen_string_literal: true
 
+
 # A namespace
 module MBTA
   module Subway
     # All of the subway lines
     class System
-      attr_reader :subway
+      attr_reader :subway, :lines
       def initialize
-        @subway = {
-          Red: Line.new('Red', ['South Station',
-                                'Park Street',
-                                'Kendall',
-                                'Central',
-                                'Harvard',
-                                'Porter',
-                                'Davis',
-                                'Alewife']),
-          Green: Line.new('Green', ['Government Center',
-                                    'Park Street',
-                                    'Boylston',
-                                    'Arlington',
-                                    'Copley',
-                                    'Hynes',
-                                    'Kenmore']),
-          Orange: Line.new('Orange', ['North Station',
-                                      'Haymarket',
-                                      'Park Street',
-                                      'State',
-                                      'Downtown Crossing',
-                                      'Chinatown',
-                                      'Back Bay',
-                                      'Forest Hills'])
-        }
+        @red_stations = ['South Station', 'Park Street', 'Kendall',
+                         'Central', 'Harvard', 'Porter', 'Davis', 'Alewife']
+        @green_stations = ['Government Center', 'Park Street', 'Boylston',
+                           'Arlington', 'Copley', 'Hynes', 'Kenmore']
+        @orange_stations = ['North Station', 'Haymarket', 'Park Street',
+                            'State', 'Downtown Crossing', 'Chinatown',
+                            'Back Bay', 'Forest Hills']
+        @lines = [Line.new('Red', @red_stations),
+                  Line.new('Green', @green_stations),
+                  Line.new('Orange', @orange_stations)]
       end
 
       def stops_between_stations(line_a, station_a, line_b, station_b)
-        if line_a == line_b
-          subway[line_a.to_sym].distance_between_stations station_a, station_b
-        else
-          start_stops = subway[line_a.to_sym].distance_from_intersection station_a
-          end_stops = subway[line_b.to_sym].distance_from_intersection station_b
-          start_stops + end_stops
-          # stops between start and intersection, then stops between
-        end
+        start_stops = line(line_a).intersection_distance station_a
+        end_stops = line(line_b).intersection_distance station_b
+        start_stops + end_stops
+      end
+
+      def line(name)
+        index = lines.index { |line| line.name == name }
+        lines[index]
       end
 
       # return true if handling multiple intersections
@@ -55,24 +41,32 @@ module MBTA
       attr_reader :stations, :name
       def initialize(name, stations)
         @name = name
-        @stations = stations
+        @stations = stations.map { |e| Station.new(e) }
       end
 
-      def distance_from_intersection(station)
-        (stations.index(station) - stations.index('Park Street')).abs
+      def intersection_distance(station)
+        (position(station) - intersection_position).abs
       end
 
-      def distance_between_stations(station1, station2)
-        (stations.index(station1) - stations.index(station2)).abs
+      def distance_between(station1, station2)
+        (position(station1) - position(station2)).abs
+      end
+
+      def position(station)
+        stations.index { |e| e.name == station }
+      end
+
+      def intersection_position
+        stations.index { |e| e.name == 'Park Street' }
       end
     end
 
     # One station
     class Station
-      attr_reader :name
+      attr_reader :name, :intersection
       def initialize(name)
         @name = name
-        @intersection
+        @intersection = name == 'Park Street'
       end
     end
   end
